c03ad541cd3713ee2a711189b6b85325d4699129
Luana <santos.luanabeatriz1995@gmail.com>
2024-01-28 13:13:12 +0000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,13 @@ import ConfigContent from "@/components/Molecule/ConfigContent/ConfigContent";
 
 export default function Home() {
   return (
-    <div className="bg-primary text-primary">
+    <div className="bg-secondary text-primary">
       <ThemeProvider initialTheme="light">
         <main>
           <ConfigContent />
           <div className="flex justify-center items-center flex-col min-h-screen">
             <h1 className="text-8xl">Em breve</h1>
-            <h4 className="text-5xl font-q">Kleytow Moreira Portfolio</h4>
+            <h4 className="text-5xl font-q text-secondary">Kleytow Moreira Portfolio</h4>
           </div>
         </main>
       </ThemeProvider>
