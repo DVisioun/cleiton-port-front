fbc8bf88e17c3d630c4c3f1d7e6384dad5262a19
Eric Kunzel <kunzel.eric@gmail.com>
2024-02-09 10:06:42 -0300

    fix: updated language params in components

--- src/app/[lang]/portfolio/page.tsx
+++ src/app/[lang]/portfolio/page.tsx
@@ -1,11 +1,15 @@
+import Header from '@/components/Molecule/Header/Header'
 import Portfolio from '@/components/Molecule/Portfolio/Portfolio'
+import { Locale } from '@/config/i18n.config'
+import ThemeProvider from '@/hooks/ThemeContext'
 import React from 'react'
 
-function page() {
+function page({ params }: { params: { lang: Locale } }) {
   return (
-    <>
+    <ThemeProvider initialTheme="light">
+      <Header lang={params.lang} bgColor="bg-primary" />
       <Portfolio />
-    </>
+    </ThemeProvider>
   )
 }
 
--- src/components/Molecule/Portfolio/Portfolio.tsx
+++ src/components/Molecule/Portfolio/Portfolio.tsx
@@ -1,21 +1,18 @@
 import React from 'react'
-import ThemeProvider from '@/hooks/ThemeContext'
 import CardPortfolioSession from '@/components/Atom/CardPortfolioSession/CardPortfolioSession'
 
 const Portfolio = () => {
   return (
-    <main className="w-full bg-[var(--white)] py-14">
-      <ThemeProvider initialTheme="light">
-        <section className="flex w-full items-center justify-between gap-6 px-4">
-          <div className="h-[.05rem] w-1/3 bg-black"></div>
-          <h2 className="font-alt text-5xl font-normal">Portfólio</h2>
-          <div className="h-[.05rem] w-1/3 bg-black"></div>
-        </section>
-        <section className="flex w-full flex-col items-center justify-center gap-6 p-4 sm-cardPortfolio-0:flex-row sm-cardPortfolio-0:flex-wrap">
-          <CardPortfolioSession />
-          <CardPortfolioSession />
-        </section>
-      </ThemeProvider>
+    <main className="mt-40 w-full bg-[var(--white)] py-14">
+      <section className="flex w-full items-center justify-between gap-6 px-4">
+        <div className="h-[.05rem] w-1/3 bg-black"></div>
+        <h2 className="font-alt text-5xl font-normal">Portfólio</h2>
+        <div className="h-[.05rem] w-1/3 bg-black"></div>
+      </section>
+      <section className="flex w-full flex-col items-center justify-center gap-6 p-4 sm-cardPortfolio-0:flex-row sm-cardPortfolio-0:flex-wrap">
+        <CardPortfolioSession />
+        <CardPortfolioSession />
+      </section>
     </main>
   )
 }
