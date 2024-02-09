1d1753908abcec2ca3a5ec37e9d9b436832622e3
Eric Kunzel <kunzel.eric@gmail.com>
2024-02-09 10:57:15 -0300

    fix: altered blog & portfolio layout and components style

---
+++ src/app/cms/page.tsx
@@ -0,0 +1,9 @@
+function Page() {
+  return (
+    <div className="bg-secondary text-primary">
+      <form></form>
+    </div>
+  )
+}
+
+export default Page
--- src/components/Molecule/Portfolio/Portfolio.tsx
+++ src/components/Molecule/Portfolio/Portfolio.tsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import CardPortfolioSession from '@/components/Atom/CardPortfolioSession/CardPortfolioSession'
+import Title from '@/components/Atom/Title/Title'
 
 const Portfolio = () => {
   return (
-    <main className="mt-40 w-full bg-[var(--white)] py-14">
-      <section className="flex w-full items-center justify-between gap-6 px-4">
-        <div className="h-[.05rem] w-1/3 bg-black"></div>
-        <h2 className="font-alt text-5xl font-normal">Portfólio</h2>
-        <div className="h-[.05rem] w-1/3 bg-black"></div>
-      </section>
+    <main className="min-h-screen w-full bg-[var(--white)] px-[80px] pt-40">
+      <Title title="Portfólio" />
       <section className="flex w-full flex-col items-center justify-center gap-6 p-4 sm-cardPortfolio-0:flex-row sm-cardPortfolio-0:flex-wrap">
         <CardPortfolioSession />
         <CardPortfolioSession />
