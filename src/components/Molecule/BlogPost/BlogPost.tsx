4e7c102e21ba0f5761f0e1a81dcfc023be091e4e
Luana <santos.luanabeatriz1995@gmail.com>
2024-03-23 13:09:12 +0000

    Layout fixes

--- src/components/Molecule/BlogPost/BlogPost.tsx
+++ src/components/Molecule/BlogPost/BlogPost.tsx
@@ -1,42 +1,42 @@
-'use client'
+"use client";
 
-import { API } from '@/@types/api'
-import { fetchBlogPosts } from '@/api/BlogPost/fetch-blog-post'
-import BlogHero from '@/components/Atom/BlogHero/BlogHero'
-import React, { useEffect, useState } from 'react'
+import { API } from "@/@types/api";
+import { fetchBlogPosts } from "@/api/BlogPost/fetch-blog-post";
+import BlogHero from "@/components/Atom/BlogHero/BlogHero";
+import React, { useEffect, useState } from "react";
 
 interface BlogPostProps {
-  id: string
+  id: string;
 }
 
 function BlogPost({ id }: BlogPostProps) {
-  const [blogPost, setBlogPost] = useState<API.BlogPostSchema[]>()
+  const [blogPost, setBlogPost] = useState<API.BlogPostSchema[]>();
 
   const handleBlogPostsFetch = async () => {
-    const response = await fetchBlogPosts()
+    const response = await fetchBlogPosts();
     if (response) {
-      setBlogPost(response.data)
+      setBlogPost(response.data);
     }
-  }
+  };
 
   useEffect(() => {
-    handleBlogPostsFetch()
-    handleFilteredPosts()
-  }, [])
+    handleBlogPostsFetch();
+    handleFilteredPosts();
+  }, []);
 
   const handleFilteredPosts = () => {
-    const filteredPosts = blogPost?.filter((post) => post.id === id)
-    setBlogPost(filteredPosts)
-  }
+    const filteredPosts = blogPost?.filter((post) => post.id === id);
+    setBlogPost(filteredPosts);
+  };
 
-  console.log(blogPost)
+  console.log(blogPost);
   return (
     <>
       {blogPost?.map((item) => {
         return (
           <div key={item.content}>
             <BlogHero imageHeader={item.image} />
-            <div className="mt-10 px-40 pb-10 sm-1:px-20">
+            <div className="mt-10 px-40 pb-10 sm-0:px-7">
               <h2 className="text-5xl sm-1:text-3xl">{item.name}</h2>
               <p>
                 <b>Ceiton Moreira</b> - <span>03/02/2024</span>
@@ -44,14 +44,14 @@ function BlogPost({ id }: BlogPostProps) {
               <div className="mt-5 h-[2px] w-[100%] bg-[var(--gold)]"></div>
               <div
                 dangerouslySetInnerHTML={{ __html: item.content }}
-                className="mt-20"
+                className="mt-10"
               />
             </div>
           </div>
-        )
+        );
       })}
     </>
-  )
+  );
 }
 
-export default BlogPost
+export default BlogPost;
