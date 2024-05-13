827d5e9b9f95dd576fa17edbd737d7ca40e5271f
Maclean <contatolucasmaclean@gmail.com>
2024-05-13 15:48:56 -0300

    feat: added responsiveness in screens > 1400px/2560px

--- src/components/Molecule/BodyPortfolioPost/BodyPortfolioPost.tsx
+++ src/components/Molecule/BodyPortfolioPost/BodyPortfolioPost.tsx
@@ -1,155 +1,159 @@
-import { API } from "@/@types/api";
-import { fetchPortfolioProjects } from "@/api/PortfolioProject/fetch-portfolio-project";
-import { fetchSoftwares } from "@/api/Software/fetch-softwares";
-import AboutCard from "@/components/Atom/AboutCard/AboutCard";
-import { softwareAtom } from "@/states/softwareAtom";
-import { useAtom } from "jotai";
-import Image from "next/image";
-import React, { use, useEffect, useState } from "react";
-import { readBase64ToFile } from "@/utils/base64-converter";
-import Link from "next/link";
-import { Locale } from "@/config/i18n.config";
+import AboutCard from '@/components/Atom/AboutCard/AboutCard'
+import Image from 'next/image'
+import React from 'react'
 
-interface PortfolioPostProps {
-  id: string;
-  lang: Locale;
-}
-
-const BodyPortfolioPost = ({ id, lang }: PortfolioPostProps) => {
-  const [portfolioPost, setPortfolioPost] =
-    useState<API.PortfolioPostSchema[]>();
-  const [portfolioOtherPosts, setPortfolioOtherPosts] =
-    useState<API.PortfolioPostSchema[]>();
-  const [softwares, setSoftwares] = useAtom(softwareAtom);
-
-  const handlePortfolioPostsFetch = async () => {
-    const response = await fetchPortfolioProjects();
-    if (response) {
-      handleFilteredPosts(response.data);
-    }
-  };
-
-  const handleFilteredPosts = (data: any) => {
-    const filteredOtherPosts = data?.filter((post) => post.id !== id);
-    console.log(filteredOtherPosts);
-    setPortfolioOtherPosts(filteredOtherPosts.slice(0, 2));
-
-    const filteredPosts = data?.filter((post) => post.id === id);
-    setPortfolioPost(filteredPosts);
-  };
-
-  const handleSoftwareFetch = async () => {
-    const response = await fetchSoftwares();
-
-    if (response) {
-      setSoftwares(response.data);
-    }
-  };
-
-  useEffect(() => {
-    handlePortfolioPostsFetch();
-    handleSoftwareFetch();
-  }, []);
-
+const BodyPortfolioPost = () => {
   return (
     <div className="flex w-full items-start justify-around md-1.5:flex-col md-1.5:items-center md-1.5:justify-center md-1.5:gap-12">
-      <section className="flex w-2/3 flex-col items-start justify-start gap-4 md-1.5:!w-full md-1.5:items-center md-2:w-1/2">
-        {portfolioPost && (
-          <div
-            dangerouslySetInnerHTML={{ __html: portfolioPost[0].content }}
-            className="mt-10"
-          />
-        )}
+      <section className="flex w-2/3 flex-col items-start justify-start gap-4 md-1.5:!w-full md-1.5:items-center md-2:w-1/2 md-3:items-center">
+        <Image
+          alt="Portfólio Image"
+          className="mt-4"
+          width={800}
+          height={450}
+          src="/images/portfolio/posts/img1.png"
+          aria-label="Portfólio Image"
+        />
+        <Image
+          alt="Portfólio Image"
+          width={800}
+          height={450}
+          src="/images/portfolio/posts/img2.png"
+          aria-label="Portfólio Image"
+        />
+        <Image
+          alt="Portfólio Image"
+          width={800}
+          height={886}
+          src="/images/portfolio/posts/img3.png"
+          aria-label="Portfólio Image"
+        />
+        <Image
+          alt="Portfólio Image"
+          width={800}
+          height={320}
+          src="/images/portfolio/posts/img4.png"
+          aria-label="Portfólio Image"
+        />
       </section>
-      <div className="flex w-[30%] flex-col items-start justify-center gap-6 sm-2.1:!flex-col sm-2.1:items-center md-1.5:!w-[82%] md-1.5:flex-row md-2:w-[35%]">
-        <div className="flex w-full flex-col items-center justify-start bg-secondary px-8 py-4 shadow-card sm-0:px-2">
+      <div className="flex w-[30%] flex-col items-start justify-center gap-6 sm-2.1:!flex-col sm-2.1:items-center md-1.5:!w-[82%] md-1.5:flex-row md-2:w-[35%] md-3:items-center">
+        <div className="flex w-full flex-col items-center justify-start bg-secondary px-8 py-4 shadow-card sm-0:px-2 md-3:!w-[385px]">
           <AboutCard />
-          {portfolioPost && (
-            <h2 className="w-full text-left text-primary">
-              {portfolioPost[0].name}
-            </h2>
-          )}
-          {portfolioPost && (
-            <p className="w-full text-left text-primary">
-              {" "}
-              {portfolioPost[0].description}
-            </p>
-          )}
+          <h2 className="w-full text-left text-primary">Geraldão</h2>
+          <p className="w-full text-left text-primary">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industrys standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged.
+          </p>
+          <section className="mt-4 flex w-full items-center justify-start gap-8">
+            <div className="flex w-20 cursor-pointer items-center justify-start gap-3 text-primary">
+              <Image
+                className="h-5 w-5 cursor-pointer"
+                src="/images/In.png"
+                alt="LinkedIn Icon"
+                width={40}
+                height={40}
+                aria-label="LinkedIn Icon"
+              ></Image>
+              Share
+            </div>
+            <div className="flex cursor-pointer items-center justify-start gap-3 text-primary">
+              <Image
+                className="h-5 w-5 cursor-pointer"
+                src="/images/x.png"
+                alt="LinkedIn Icon"
+                width={40}
+                height={40}
+                aria-label="LinkedIn Icon"
+              ></Image>
+              Share
+            </div>
+          </section>
           <section className="m-10 flex w-full flex-col items-center justify-center">
             <h3 className="w-full text-left text-primary">Softwares</h3>
-            <div className="flex w-full flex-wrap items-center justify-start gap-4">
-              {portfolioPost &&
-                portfolioPost[0].softwares.map((softwarePost) => {
-                  return softwares.map(async (software) => {
-                    if (software.id === softwarePost.software_id) {
-                      const imageCoverAux = await readBase64ToFile(
-                        software.image,
-                      );
-                      const previewURL = URL.createObjectURL(imageCoverAux);
-                      return (
-                        <div className="flex cursor-default items-center justify-center gap-2 rounded bg-content px-3 py-1 shadow-software">
-                          <Image
-                            className="h-8 w-8"
-                            src={previewURL}
-                            alt={software.name}
-                            aria-label={`${software.name} Icon`}
-                            width={40}
-                            height={40}
-                          ></Image>
-                          <p className="text-white">{software.name}</p>
-                        </div>
-                      );
-                    }
-                  });
-                })}
+            <div className="flex w-full items-center justify-start gap-4">
+              <div className="flex cursor-default items-center justify-center gap-2 rounded bg-content px-3 py-1 shadow-software">
+                <Image
+                  className="h-8 w-8"
+                  src="/images/softwares/blender.png"
+                  alt="Blender Icon"
+                  aria-label="Blender Icon"
+                  width={40}
+                  height={40}
+                ></Image>
+                <p className="text-white">Blender</p>
+              </div>
+              <div className="flex cursor-default items-center justify-center gap-2 rounded bg-content px-3 py-1 shadow-software">
+                <Image
+                  className="h-8 w-8"
+                  src="/images/softwares/photoshop.png"
+                  alt="Photoshop Icon"
+                  aria-label="Photoshop Icon"
+                  width={40}
+                  height={40}
+                ></Image>
+                <p className="text-white">Photoshop</p>
+              </div>
             </div>
           </section>
         </div>
-        {portfolioOtherPosts && (
-          <div className="flex w-full flex-col items-center justify-start gap-8 bg-secondary px-12 py-10 shadow-card sm-0:px-4 sm-2:!w-full sm-2.1:w-1/2">
-            <h3 className="m-0 w-full text-left text-3xl text-primary">
-              Outras postagens
-            </h3>
-            {portfolioOtherPosts.map(async (post) => {
-              const imageCoverAux = await readBase64ToFile(post.image);
-              const previewURL = URL.createObjectURL(imageCoverAux);
-
-              return (
-                <Link
-                  href={`/${lang}/portfolio/posts/${post.id}`}
-                  className="text-primary no-underline hover:text-primary"
-                >
-                <div className="relative w-full">
-                  <Image
-                    src={previewURL}
-                    alt="Another post"
-                    width={300}
-                    height={300}
-                    aria-label="Another post"
-                  ></Image>
-                  <div className="absolute bottom-0 flex w-full items-center justify-start gap-3 bg-opacity p-4">
-                    <Image
-                      className="rounded-full"
-                      alt="Cleiton Avatar"
-                      aria-label="Cleiton Avatar"
-                      src="/images/avatar.jpg"
-                      width={56}
-                      height={56}
-                    ></Image>
-                    <div className="flex flex-col items-start justify-center">
-                      <h2 className="m-0 text-xl text-primary">{post.name}</h2>
-                      <h6 className="text-secondary">Cleiton Moreira</h6>
-                    </div>
-                  </div>
-                </div>
-                </Link>
-              );
-            })}
+        <div className="flex w-full flex-col items-center justify-start gap-8 bg-secondary px-12 py-10 shadow-card sm-0:px-4 sm-2:!w-full sm-2.1:w-1/2 md-3:!w-[385px]">
+          <h3 className="m-0 w-full text-left text-3xl text-primary">
+            Outras postagens
+          </h3>
+          <div className="relative w-full">
+            <Image
+              src="/images/anotherr.png"
+              alt="Another post"
+              width={300}
+              height={300}
+              aria-label="Another post"
+            ></Image>
+            <div className="absolute bottom-0 flex w-full items-center justify-start gap-3 bg-opacity p-4">
+              <Image
+                className="rounded-full"
+                alt="Cleiton Avatar"
+                aria-label="Cleiton Avatar"
+                src="/images/avatar.jpg"
+                width={56}
+                height={56}
+              ></Image>
+              <div className="flex flex-col items-start justify-center">
+                <h2 className="m-0 text-xl text-primary">Nome do personagem</h2>
+                <h6 className="text-secondary">Cleiton Moreira</h6>
+              </div>
+            </div>
           </div>
-        )}
+          <div className="relative w-full">
+            <Image
+              src="/images/anotherr.png"
+              alt="Another post"
+              width={300}
+              height={300}
+              aria-label="Another post"
+            ></Image>
+            <div className="absolute bottom-0 flex w-full items-center justify-start gap-3 bg-opacity p-4">
+              <Image
+                className="rounded-full"
+                alt="Cleiton Avatar"
+                aria-label="Cleiton Avatar"
+                src="/images/avatar.jpg"
+                width={56}
+                height={56}
+              ></Image>
+              <div className="flex flex-col items-start justify-center">
+                <h2 className="m-0 text-xl text-primary">Nome do personagem</h2>
+                <h6 className="text-secondary">Cleiton Moreira</h6>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default BodyPortfolioPost;
+export default BodyPortfolioPost
