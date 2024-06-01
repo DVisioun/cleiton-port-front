d5badd24054c05af1ee14cbad572c21a90cc3d40
Eric Kunzel <kunzel.eric@gmail.com>
2024-06-01 11:36:44 -0300

    fix: removed navbar lang in links hrefs

--- src/components/Atom/Navbar/Navbar.tsx
+++ src/components/Atom/Navbar/Navbar.tsx
@@ -19,7 +19,7 @@ function Navbar({ lang }: NavBarProps) {
         <DropdownMenu className="!w-28 sm-1:!-translate-x-20">
           <DropdownItem>
             <Link
-              href={`/${lang}/home`}
+              href={`/home`}
               className="text-lg text-primary duration-300 hover:text-hover hover:underline active:text-hover active:underline sm-0.1:text-sm sm-1:text-base"
             >
               Home
@@ -27,7 +27,7 @@ function Navbar({ lang }: NavBarProps) {
           </DropdownItem>
           <DropdownItem>
             <Link
-              href={`/${lang}/about`}
+              href={`/about`}
               className="text-lg text-primary duration-300 hover:text-hover hover:underline sm-0.1:text-sm sm-1:text-base"
             >
               About
@@ -35,7 +35,7 @@ function Navbar({ lang }: NavBarProps) {
           </DropdownItem>
           <DropdownItem>
             <Link
-              href={`/${lang}/portfolio`}
+              href={`/portfolio`}
               className="text-lg text-primary duration-300 hover:text-hover hover:underline sm-0.1:text-sm sm-1:text-base"
             >
               Portfolio
@@ -43,7 +43,7 @@ function Navbar({ lang }: NavBarProps) {
           </DropdownItem>
           <DropdownItem>
             <Link
-              href={`/${lang}/blog`}
+              href={`/blog`}
               className="text-lg text-primary duration-300 hover:text-hover hover:underline sm-0.1:text-sm sm-1:text-base"
             >
               Blog
@@ -52,25 +52,25 @@ function Navbar({ lang }: NavBarProps) {
         </DropdownMenu>
       </Dropdown>
       <Link
-        href={`/${lang}/home`}
+        href={`/home`}
         className="text-xl text-primary duration-300 hover:text-hover hover:underline active:text-hover active:underline sm-1:hidden"
       >
         Home
       </Link>
       <Link
-        href={`/${lang}/about`}
+        href={`/about`}
         className="text-xl text-primary duration-300 hover:text-hover hover:underline sm-1:hidden"
       >
         About
       </Link>
       <Link
-        href={`/${lang}/portfolio`}
+        href={`/portfolio`}
         className="text-xl text-primary duration-300 hover:text-hover hover:underline sm-1:hidden"
       >
         Portfolio
       </Link>
       <Link
-        href={`/${lang}/blog`}
+        href={`/blog`}
         className="text-xl text-primary duration-300 hover:text-hover hover:underline sm-1:hidden"
       >
         Blog
